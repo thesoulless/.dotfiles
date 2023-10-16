85cfb9b4113430b4156d2bc572142f518dc8ba1d
Hamed Nemati <hitech.innovative@gmail.com>
2023-10-16 11:09:23 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+for script in $(ls ./scripts)
+do
+    echo "running ./installs/$script"
+    ./installs/$script
+done
+
+stowit() {
+    echo "stow $1"
+    stow -D $1
+    stow $1
+}
+
+stowit "nvim"
