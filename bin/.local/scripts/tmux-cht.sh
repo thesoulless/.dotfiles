02860575ac9bb4e3c264c996e628f3b5bb53c1ed
Hamed Nemati <hitech.innovative@gmail.com>
2024-07-12 17:49:14 +0200

    Integrate chat help

--- bin/.local/scripts/tmux-cht.sh
+++ bin/.local/scripts/tmux-cht.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+# selected='cc \n' + `cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+all=`cat ~/.tmux-cht-languages ~/.tmux-cht-command`
+all+=("chat")
+selected="$(printf "%s\n" "${all[@]}" | fzf)"
 if [[ -z $selected ]]; then
     exit 0
 fi
@@ -9,6 +12,8 @@ read -p "Enter Query: " query
 if grep -qs "$selected" ~/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
-else
+elif grep -qs "$selected" ~/.tmux-cht-command; then
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+else
+    tmux neww bash -c "chatgpt $query | less"
 fi
