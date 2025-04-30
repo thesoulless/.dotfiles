092847e43fb871afd2d906e26b09adefb9445649
Hamed Nemati <h.nemati.ziabari@mediahuis.nl>
2025-04-30 18:11:32 +0200

    Load lazy

--- nvim/.config/nvim/lua/hamed/lazy/mcphub.lua
+++ nvim/.config/nvim/lua/hamed/lazy/mcphub.lua
@@ -1,9 +1,9 @@
 return {
     "ravitemer/mcphub.nvim",
     dependencies = {
-        "nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+        "nvim-lua/plenary.nvim",             -- Required for Job and HTTP requests
     },
-    -- cmd = "MCPHub", -- lazily start the hub when `MCPHub` is called
+    cmd = "MCPHub",                          -- lazily start the hub when `MCPHub` is called
     build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
     config = function()
         local config = vim.fn.expand("~/mcpservers.json")
